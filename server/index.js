7dfb8cb6cefc5641c785ec6b898919fa1f6e9a34
Mike Champagne <geauxdrum@gmail.com>
2020-09-04 11:30:45 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const axios = require('axios');
+require('newrelic');
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.json())
+
+
+app.get('/products/:company/:material/:product/:color', (req, res) => {
+  let company = req.params.company;
+  let material = req.params.material;
+  let product = req.params.product;
+  let color = req.params.color;
+
+  axios.get(`http://localhost:3001/products/${company}/${material}/${product}/${color}`)
+    .then(response => res.send(response.data))
+})
 
 
 app.listen(3003, () => console.log('Server running on port 3003'))
---
+++ server/test.js
@@ -0,0 +1,23 @@
+import { sleep } from "k6";
+import http from "k6/http";
+
+export const options = {
+  stages: [
+    { duration: "1m", target: 20 },
+    //{ duration: "1m", target: 1500 },
+    //{ duration: "3m", target: 2000 },
+    //{ duration: "1m", target: 250 },
+  ]
+};
+
+export default function main() {
+  let response;
+
+  // script here []
+  response = http.get(
+    "http://127.0.0.1:3001/products/Toy/Frozen/violet/Cheese"
+  );
+
+  // Automatically added sleep
+  sleep(1);
+}
